0d5dcf5feeba92d0fb2ef7e22498b1aadb1d49c4
ErmanCKMKC <ermanc.is0@gmail.com>
2026-02-04 13:27:12 +0300

    Update question5.sql

--- question5.sql
+++ question5.sql
@@ -1,17 +1,23 @@
-with empty_department as(
-    SELECT d.department_name, 
-    Listagg(substr(department_name, -LEVEL,1), ’’)
-	Within group (order by level) as reversed
-FROM hr.departments d
-LEFT JOIN hr.employees e 
-    ON d.department_id = e.department_id
-Where e.employee_id is null
-Connect by level <= length(department_name)
-And prior department_name = department_name
-And prior sys_guid() is not null
-GROUP BY d.department_name
+WITH empty_departments AS (
+    SELECT d.department_name
+    FROM hr.departments d
+    LEFT JOIN hr.employees e
+        ON d.department_id = e.department_id
+    WHERE e.employee_id IS NULL
+),
+reversed_departments AS (
+    SELECT department_name,
+           LISTAGG(SUBSTR(department_name, -LEVEL, 1), '')
+               WITHIN GROUP (ORDER BY LEVEL) AS reversed_name
+    FROM empty_departments
+    CONNECT BY LEVEL <= LENGTH(department_name)
+       AND PRIOR department_name = department_name
+       AND PRIOR SYS_GUID() IS NOT NULL
+    GROUP BY department_name
 )
 SELECT *
-From reversed;
+FROM reversed_departments;
+
 
 --Q5:(Question 3 cont.) Return reverse of those department names. (For example => input = ‘Benefits’, output = ‘stifeneB’). (HINT: do not use reverse function)
+
