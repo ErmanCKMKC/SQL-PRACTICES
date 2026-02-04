ba8d9c768789726a071f36e90ad80c978bab31ef
ErmanCKMKC <ermanc.is0@gmail.com>
2026-02-04 10:34:45 +0300

    Update question4_without_reverse.sql

--- question4_without_reverse.sql
+++ question4_without_reverse.sql
@@ -6,7 +6,7 @@ LEFT JOIN hr.employees e
     ON d.department_id = e.department_id
 GROUP BY d.department_name
 )
-SELECT department_name,calisan_sayisi
+SELECT department_name,calisan_sayisi,
 substr(department_name,-1,1)||
 substr(department_name,-2,1)||
 substr(department_name,-3,1) as output
@@ -14,3 +14,4 @@ from empty_department
 where calisan_sayisi = 0;
 
 --Q4:(Question 3 cont.) Return the string between 1st letter from the end and 3th letter from the end.  . (For example => input = ‘Benefits’, output = ‘sti’).
+
